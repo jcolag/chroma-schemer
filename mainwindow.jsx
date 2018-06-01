f8075e53dd80b3befc6f1098b4f126c1f822456a
John Colagioia <john@colagioia.net>
2018-06-01 06:34:46 -0400

    Rough out minimal user interface

--- mainwindow.jsx
+++ mainwindow.jsx
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'; // import from react
-import { render, Window, App } from 'proton-native'; // import the proton-native components
+import React, { Component } from 'react';
+import { render, App, ColorButton, Grid, Text, Window } from 'proton-native';
 
 export default class MainWindow extends Component {
-  render() { // all Components must have a render method
+  updateColor(color) {
+    console.log(color);
+  }
+
+  render() {
     return (
-      <App> // you must always include App around everything
+      <App>
         <Window title="Color Schemer" size={{w: 500, h: 300}} menuBar={false}>
+          <Grid padded={true}>
+        	<Text row={0} column={0}>Select a primary color:</Text>
+        	<ColorButton color="#007700" onChange={this.updateColor.bind(this)} row={1} column={0} />
             {/* all your other components go here*/}
+          </Grid>
         </Window>
       </App>
     );
