32f5592c84e67c16cd8e5603eee13e361789b69d
John Colagioia <john@colagioia.net>
2018-06-05 05:57:44 -0400

    Add RGB to HSL conversion on updating color

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -23,6 +23,34 @@ const get = (state = defaultState, action) => {
       return { ...state, theta: action.angle };
     case actionConsts.SET_COLOR:
       let c = action.color;
+      let r = c.r / 255;
+      let g = c.g / 255;
+      let b = c.b / 255;
+      let min = Math.min(r, g, b);
+      let max = Math.max(r, g, b);
+      c.l = (max + min) / 2;
+      if (min === max) {
+        c.h = 0;
+        c.s = 0;
+      } else {
+        let d = max - min;
+        c.s = c.l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+          case r:
+            c.h = (g - b) / d + (g < b ? 6 : 0);
+            break;
+          case g:
+            c.h = (b - r) / d + 2;
+            break;
+          case b:
+            c.h = (r - g) / d + 4;
+            break;
+        }
+        c.h /= 6;
+      }
+      c.h *= 360;
+      c.s *= 100;
+      c.l *= 100;
       return { ...state, color: c };
     case actionConsts.SET_SCHEME:
       return { ...state, scheme: Number(action.scheme) };
