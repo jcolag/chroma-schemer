07f8b08f504e9816ff241aa92ca9e3a26758d72d
John Colagioia <john@colagioia.net>
2018-06-12 06:16:12 -0400

    Minor fixes to main code

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+import { render } from 'proton-native';
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './src/reducers';
-import { render } from 'proton-native';
-import MainWindow from './src/components/mainwindow';
+import { MainWindow } from './src/components/mainwindow';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>
     <MainWindow />
-  </Provider>
-);
+  </Provider>);
